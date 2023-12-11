3fef75137a6474bfc31f3600062c6db63cef260a
Suraj7280 <surajsharma95595@gmail.com>
2023-12-11 06:37:59 +0530

    Added my project

---
+++ src/main/java/com/uniquedeveloper/registration/RegistrationServlet.java
@@ -0,0 +1,59 @@
+package com.uniquedeveloper.registration;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/register")
+public class RegistrationServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String username=request.getParameter("name");
+		String uemail=request.getParameter("email");
+		String upwd=request.getParameter("pass");
+		String umobile=request.getParameter("contact");
+		RequestDispatcher dispatcher =null;
+		Connection con =null;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/cryptoUser?useSSL=false","root","surajS@123");
+			PreparedStatement pst =con.prepareStatement("insert into users(username,upwd,uemail,umobile) values(?,?,?,?)");
+			pst.setString(1, username);
+			pst.setString(2, upwd);
+			pst.setString(3, uemail);
+			pst.setString(4, umobile);
+			
+			int rowCount = pst.executeUpdate();	
+			dispatcher = request.getRequestDispatcher("registration.jsp");
+			if(rowCount>0) {
+				request.setAttribute("Status","success");
+			}else {
+				request.setAttribute("Status","failed");
+			}
+			dispatcher.forward(request, response);
+		}catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				con.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+	}
+
+}
